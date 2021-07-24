14a512e53c2fa0300cb589c8ac5efc213533d01c
SantoshK96 <santoshkumarp1996@gmail.com>
2021-07-24 23:29:28 +0530

    Navbar implemented with styling

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import "./navbar.scss";
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="right">
+                    <Search className="icon" />
+                    <span>KID</span>
+                    <Notifications className="icon" />
+                    <img src="https://images.pexels.com/photos/4776/man-sunglasses-art-graffiti.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+                    <div className="profile">
+                        <ArrowDropDown className="icon" />
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
