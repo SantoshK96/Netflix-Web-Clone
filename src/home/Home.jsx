17f745be804794964c55dd7dc60db299b8698fe9
SantoshK96 <santoshkumarp1996@gmail.com>
2021-07-25 00:45:38 +0530

    Navbar background gradient when offsetY 0, fixed when scrolling

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     return (
         <div className="home">
             <Navbar/>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
         </div>
     )
 }
